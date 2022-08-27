a72aeffb6f28b9552b5ce761cef3b96df1ff6cf6
annie-82 <92351283+annie-82@users.noreply.github.com>
2022-08-27 05:43:24 -0500

    Update M7Python2.py

--- M7Python2.py
+++ M7Python2.py
@@ -3,8 +3,8 @@
 number = int(input('Enter a number to check in range(1, 10) : '))
 
 # check if 'number' lies between 1 and 10
-if number >= 1 and number <= 10:
-    # if it is in range, print apt message
+if number >= 1 and number <= 10:                  # SUPPOSED TO USE IN, NOT IN OPERATOR
+    # if it is in range, print apt message        #if number in range(1, 11):
     print('The number is in range(1,10)')
 
 else:
