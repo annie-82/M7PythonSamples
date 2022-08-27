1c7a0b6e767756807517f6e5774618c5c367d7e0
annie-82 <92351283+annie-82@users.noreply.github.com>
2022-08-27 05:46:15 -0500

    Update M7Python2.py

--- M7Python2.py
+++ M7Python2.py
@@ -1,9 +1,15 @@
 # -------------------- PROBLEM 2 --------------------
 # prompt the user to enter a number
+
+'''
+# Problem 2: Write a Python function to check whether a number is in a given range. Use range(1,10).
+# Print whether the number is in or not in the range.
+'''
+
 number = int(input('Enter a number to check in range(1, 10) : '))
 
 # check if 'number' lies between 1 and 10
-if number >= 1 and number <= 10:
+if number >= 1 and number <= 10:                #if number in range(1, 10):
     # if it is in range, print apt message
     print('The number is in range(1,10)')
 
